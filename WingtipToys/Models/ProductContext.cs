4e6aae480934f7bfbe74cfc06c40867f7a42c0f3
BoChristensen <bo_christensen@outlook.dk>
2019-03-19 00:43:18 +0100

    Create-the-Data-Access-Layer Done

---
+++ WingtipToys/Models/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿//Access all the core functionality of Entity Framework(query,insert,update amd delete data
+using System.Data.Entity;
+namespace WingtipToys.Models
+{
+    public class ProductContext : DbContext
+    {
+        //This class fetch, Storing and updating Product class instance in the database.
+        //ProductContext class derives form the DBcontext case class from Entity Framework
+        public ProductContext() : base("WingtipToys")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
